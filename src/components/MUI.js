d38ae4e439120f6488bd5f837e5804465086c8d9
l33th <l33th@protonmail.ch>
2021-10-02 06:43:48 +0200

    Added Icons

--- src/components/MUI.js
+++ src/components/MUI.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import SaveIcon from '@material-ui/icons/Save';
 
 const MUI = () => {
 	return (
-		<Button size='large' variant='contained' color='secondary'>
+		<Button startIcon={<SaveIcon />} size='large' variant='contained' color='secondary'>
 			Hello World
 		</Button>
 	);
