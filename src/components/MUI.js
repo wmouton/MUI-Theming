5dbc0d5cc43e8b94ab8a153dcfab435c6a183071
l33th <l33th@protonmail.ch>
2021-10-02 06:57:29 +0200

    Checkbox

--- src/components/MUI.js
+++ src/components/MUI.js
@@ -3,24 +3,29 @@ import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import SaveIcon from "@material-ui/icons/Save";
 import DeleteIcon from "@material-ui/icons/Delete";
+import Checkbox from "@material-ui/core/Checkbox";
 
+const CheckboxExample = () => {
+	return (
+		<div>
+			<Checkbox />
+		</div>
+	);
+};
 
 const MUI = () => {
 	return (
-		<ButtonGroup variant='contained' color='primary'>
-			<Button
-				startIcon={<SaveIcon />}
-				size='large'
-			>
-				Save
-			</Button>
-			<Button
-				startIcon={<DeleteIcon />}
-				size='large'
-			>
-				Discard
-			</Button>
-		</ButtonGroup>
+		<>
+			<CheckboxExample />
+			<ButtonGroup variant='contained' color='primary'>
+				<Button startIcon={<SaveIcon />} size='large'>
+					Save
+				</Button>
+				<Button startIcon={<DeleteIcon />} size='large'>
+					Discard
+				</Button>
+			</ButtonGroup>
+		</>
 	);
 };
 
