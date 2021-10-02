4c232036c81ae98d68683192e51abfcaa56fcad1
l33th <l33th@protonmail.ch>
2021-10-02 06:38:48 +0200

    MUI Button

---
+++ src/components/MUI.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+const MUI = () => {
+	return (
+		<Button size='large' variant='contained' color='secondary'>
+			Hello World
+		</Button>
+	);
+};
+
+export default MUI;
