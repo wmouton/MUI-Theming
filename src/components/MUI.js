ef44928c1f2b9217970fcfdc3766de4c4c1928c8
l33th <l33th@protonmail.ch>
2021-10-02 06:50:17 +0200

    Save and Discard Icons

--- src/components/MUI.js
+++ src/components/MUI.js
@@ -2,17 +2,34 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
+
 
 const MUI = () => {
 	return (
 		<ButtonGroup>
+			<Button
+				size='large'
+				variant='contained'
+				color='secondary'
+			>
+				Hello World
+			</Button>
 			<Button
 				startIcon={<SaveIcon />}
 				size='large'
 				variant='contained'
 				color='secondary'
 			>
-				Hello World
+				Save
+			</Button>
+			<Button
+				startIcon={<DeleteIcon />}
+				size='large'
+				variant='contained'
+				color='secondary'
+			>
+				Discard
 			</Button>
 		</ButtonGroup>
 	);
