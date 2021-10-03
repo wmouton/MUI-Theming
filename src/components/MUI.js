4daccd835c2bd9f40335a4e900bc757ae37e0892
l33th <l33th@protonmail.ch>
2021-10-03 05:48:48 +0200

    makeStyles Button

--- src/components/MUI.js
+++ src/components/MUI.js
@@ -6,6 +6,23 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import Checkbox from "@material-ui/core/Checkbox";
 import { FormControlLabel } from "@material-ui/core";
 import { TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+	root: {
+		background: "linear-gradient(45deg, #2a4d3f, #1b878f)",
+		border: 0,
+		margin: 0,
+		borderRadius: 15,
+		color: "white",
+		padding: "0 30px",
+	},
+});
+
+const ButtonStyled = () => {
+	const classes = useStyles();
+	return <Button className={classes.root}>Test Styled Button</Button>;
+};
 
 const CheckboxExample = () => {
 	const [checked, setChecked] = React.useState(true);
@@ -30,8 +47,15 @@ const CheckboxExample = () => {
 const MUI = () => {
 	return (
 		<>
+		<ButtonStyled />
 			Email:
-			<TextField variant='filled' color='secondary' type='email' label='Email' placeholder='test@test.com' />
+			<TextField
+				variant='filled'
+				color='secondary'
+				type='email'
+				label='Email'
+				placeholder='test@test.com'
+			/>
 			Date:
 			<TextField variant='outlined' color='secondary' type='date' />
 			<CheckboxExample />
